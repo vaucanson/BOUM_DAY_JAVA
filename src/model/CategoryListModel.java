e5a2d752db3ec61efc138b0125ac3d0e6db5b505
dav <moi@mondomaine.tld>
2018-03-12 15:33:59 +0100

    bla

--- src/model/CategoryListModel.java
+++ src/model/CategoryListModel.java
@@ -3,6 +3,7 @@ package model;
 import dao.CategoryManagerOld;
 import entity.Category;
 import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.AbstractListModel;
 
 /**
@@ -14,7 +15,7 @@ public class CategoryListModel extends AbstractListModel<Category> {
     
     public CategoryListModel()
     {
-        cList = new ArrayList(Category.values());
+        cList = new ArrayList<Category>(Arrays.asList(Category.values()));
     }
 
     @Override

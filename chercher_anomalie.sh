ab4daeac0c20e2daf657949acd480464710b3159
dav <moi@mondomaine.tld>
2018-03-14 15:03:37 +0100

    suppression des orig et bak

--- chercher_anomalie.sh
+++ chercher_anomalie.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
+rm src/*/*orig
+rm src/*/*bak
+rm src/*/*/*orig
+rm src/*/*/*bak
+rm src/*/*/*/*orig
+rm src/*/*/*/*bak
+
 grep "<<" src/*/* 2>/dev/null
 grep "<<" src/*/*/* 2>/dev/null
 grep "<<" src/*/*/*/* 2>/dev/null

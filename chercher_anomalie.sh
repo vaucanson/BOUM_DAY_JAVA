13d97ed3baf6fcd4787a742a51048da5cda22bfe
dav <moi@mondomaine.tld>
2018-03-14 15:33:40 +0100

    amélioration du chercher_anomalie

--- chercher_anomalie.sh
+++ chercher_anomalie.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 printf "SUPPRESSION DES FICHIERS INDÉSIRÉS :\n"
-rm -vi src/*/*orig
-rm -vi src/*/*bak
-rm -vi src/*/*/*orig
-rm -vi src/*/*/*bak
-rm -vi src/*/*/*/*orig
-rm -vi src/*/*/*/*bak
+rm -i src/*/*orig
+rm -i src/*/*bak
+rm -i src/*/*/*orig
+rm -i src/*/*/*bak
+rm -i src/*/*/*/*orig
+rm -i src/*/*/*/*bak
+rm -i src/*/*/*/*/*bak
+rm -i src/*/*/*/*/*orig
 
 printf "\nRECHERCHE DE FICHIERS CONTENANT DES ANOMALIES :\n"
 grep "<<" src/*/* 2>/dev/null
 grep "<<" src/*/*/* 2>/dev/null
 grep "<<" src/*/*/*/* 2>/dev/null
+grep "<<" src/*/*/*/*/* 2>/dev/null

2262c552be6dec0655a00325a05c3794c9433a19
dav <moi@mondomaine.tld>
2018-03-14 16:17:40 +0100

    amélioration du chercher_anomalie

--- chercher_anomalie.sh
+++ chercher_anomalie.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 printf "SUPPRESSION DES FICHIERS INDÉSIRÉS :\n"
-rm -i src/*/*orig
-rm -i src/*/*bak
-rm -i src/*/*/*orig
-rm -i src/*/*/*bak
-rm -i src/*/*/*/*orig
-rm -i src/*/*/*/*bak
-rm -i src/*/*/*/*/*bak
-rm -i src/*/*/*/*/*orig
+rm -v src/*/*orig 2>/dev/null
+rm -v src/*/*bak 2>/dev/null
+rm -v src/*/*/*orig 2>/dev/null
+rm -v src/*/*/*bak 2>/dev/null
+rm -v src/*/*/*/*orig 2>/dev/null
+rm -v src/*/*/*/*bak 2>/dev/null
+rm -v src/*/*/*/*/*bak 2>/dev/null
+rm -v src/*/*/*/*/*orig 2>/dev/null
 
 printf "\nRECHERCHE DE FICHIERS CONTENANT DES ANOMALIES :\n"
-grep "<<" src/*/* 2>/dev/null
-grep "<<" src/*/*/* 2>/dev/null
-grep "<<" src/*/*/*/* 2>/dev/null
-grep "<<" src/*/*/*/*/* 2>/dev/null
+grep "<<" src/*/* 2>/dev/null 
+grep "<<" src/*/*/* 2>/dev/null 
+grep "<<" src/*/*/*/* 2>/dev/null 
+grep "<<" src/*/*/*/*/* 2>/dev/null 
